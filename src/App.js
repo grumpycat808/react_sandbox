d1ab934235f5806ef61e549a3e265d9eb4c9f3b6
catli-unbounce <catherine.li@unbounce.com>
2024-02-01 13:44:04 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, { useEffect, useState } from "react";
 import { getData } from "./getData";
 export default function App(props) {
   const [messages, setMessages] = useState([]);
-  useEffect(() => {
-    getData().then((response) => setMessages(response.data));
-    console.log("messages", messages);
-  }, []);
+  // const makeRequest = () => {
+  //   getData().then((response) => setMessages(response.data));
+  // };
+  // useEffect(() => {
+  //   makeRequest();
+  // }, []);
+
+  let i = 1;
+
+  function incrementBy(value) {
+    i += value;
+    return i;
+  }
+
+  function once(func) {
+    let invoked = false;
+
+    return func;
+  }
+
+  const incrementByOnce = once(incrementBy);
+  let one = incrementByOnce(2);
+  let two = incrementByOnce(3);
+  console.log("one", one);
+  console.log("two", two);
   return (
     <div className="App">
       <h1>Test</h1>
-      {messages && messages.length > 0 && <p>{messages[0].name}</p>}
     </div>
   );
 }
--- src/getData.js
+++ src/getData.js
@@ -1,11 +1,4 @@
 import axios from "axios";
-export const getData = () => {
-  return new Promise((resolve, reject) => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then(function (response) {
-        // console.log(response);
-        resolve(response);
-      });
-  });
+export const getData = async () => {
+  return await axios.get("https://jsonplaceholder.typicode.com/users");
 };
